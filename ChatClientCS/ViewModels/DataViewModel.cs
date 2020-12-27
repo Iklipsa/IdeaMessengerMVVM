8efbf1de2fd61af008f4c482890a9c5c90d4298a
Iklipsa <57861703+Golubev76ru@users.noreply.github.com>
2020-12-28 00:00:20 +0300

    Finish Version

---
+++ ChatClientCS/ViewModels/DataViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Drawing;
+using ChatClientCS.Services;
+using ChatClientCS.Enums;
+using ChatClientCS.Models;
+using ChatClientCS.Commands;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Diagnostics;
+using System.Reactive.Linq;
+
+namespace ChatClientCS.ViewModels
+{
+    public class HouseViewModel
+    {
+        public HouseViewModel()
+        {
+
+        }
+    }
+}
